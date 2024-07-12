167b544305fddd1292643c8005776607ad3219f9
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-07-12 06:00:30 +0300

    added src link for images

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ const rockBtn = document.querySelector(".rockBtn");
 const paperBtn = document.querySelector(".paperBtn");
 const scissorBtn = document.querySelector(".scissorsBtn");
 
-rockBtn.innerHTML = "<img src='/img/white_circle.png' >";
-paperBtn.innerHTML = "<img src='/img/scroll (1).png' >";
-scissorBtn.innerHTML = "<img src='/img/scissors.png' >";
+rockBtn.innerHTML = "<img src='https://images.app.goo.gl/i4MAUbCJkpgmB2T98' >";
+paperBtn.innerHTML = "<img src='https://images.app.goo.gl/rFcDu5Btxng8W8PXA' >";
+scissorBtn.innerHTML = "<img src='https://images.app.goo.gl/V8ftrNmvrAEnjFat9' >";
 
 rockBtn.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
